c3b9cc127e6b28c1949ee6af50231e264a601c4b
Pablo Gabriel Velasquez Gonzalez <pvelasquez@ubiobio.cl>
2021-08-30 23:14:45 -0400

    Agrega contenido de fondo al modulo contenedor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CuentaComponent } from './cuenta/cuenta.component';
+import { ResumenComponent } from './resumen/resumen.component';
+import { ContenedorComponent } from './contenedor/contenedor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CuentaComponent,
+    ResumenComponent,
+    ContenedorComponent
   ],
   imports: [
     BrowserModule
